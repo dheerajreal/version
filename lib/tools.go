f131cffafaeff9f0bebb714b56d2e0c4607626b0
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2026-02-03 14:15:39 +0530

    added more stuff (not tested)

--- lib/tools.go
+++ lib/tools.go
@@ -45,12 +45,18 @@ var Tools = []Tool{
 	{"Rust", "rustc", []string{"--version"}},
 	{"Java", "java", []string{"--version"}},
 	{"Ruby", "ruby", []string{"--version"}},
+	{"PHP", "php", []string{"-r", "echo phpversion();"}},
+	{"Swift", "swift", []string{"--version"}},
+	{"Kotlin", "kotlin", []string{"-version"}},
+	{"Dart", "dart", []string{"--version"}},
 	{"Perl", "perl", []string{"-v"}},
+	{"Lua", "lua", []string{"-v"}},
 	{"Elixir", "elixir", []string{"--version"}},
+	{"R", "R", []string{"--version"}},
+	{"Julia", "julia", []string{"--version"}},
 	{"C / C++", "gcc", []string{"--version"}},
 	{"Clang", "clang", []string{"--version"}},
-
-
+	{"Zig", "zig", []string{"version"}},
 	// Package managers
 	{"npm", "npm", []string{"--version"}},
 	{"yarn", "yarn", []string{"--version"}},
@@ -60,21 +66,43 @@ var Tools = []Tool{
 	{"uv", "uv", []string{"--version"}},
 	{"cargo", "cargo", []string{"--version"}},
 	{"gem", "gem", []string{"--version"}},
+	{"composer", "composer", []string{"--version"}},
 	{"Homebrew", "brew", []string{"--version"}},
-
-
+	{"luarocks", "luarocks", []string{"--version"}},
+	{"opam", "opam", []string{"--version"}},
+	{"pub", "pub", []string{"--version"}},
 	// Dev tools
 	{"Git", "git", []string{"--version"}},
 	{"Docker", "docker", []string{"--version"}},
 	{"kubectl", "kubectl", []string{"version", "--client"}},
-
+	{"Helm", "helm", []string{"version"}},
+	{"Terraform", "terraform", []string{"version"}},
+	{"Ansible", "ansible", []string{"--version"}},
+	{"Vagrant", "vagrant", []string{"--version"}},
+	{"Make", "make", []string{"--version"}},
+	{"CMake", "cmake", []string{"--version"}},
+	{"Gradle", "gradle", []string{"--version"}},
+	{"Maven", "mvn", []string{"--version"}},
+	{"ESLint", "eslint", []string{"--version"}},
+	{"Prettier", "prettier", []string{"--version"}},
 	{"Black", "black", []string{"--version"}},
 	{"Ruff", "ruff", []string{"--version"}},
 	{"SQLite", "sqlite3", []string{"--version"}},
 	{"jq", "jq", []string{"--version"}},
 	{"curl", "curl", []string{"--version"}},
-
+	// Cloud
+	{"AWS CLI", "aws", []string{"--version"}},
+	{"GCloud", "gcloud", []string{"version"}},
+	{"Azure CLI", "az", []string{"version"}},
+	{"Fly.io", "flyctl", []string{"version"}},
+	{"Vercel", "vercel", []string{"--version"}},
+	{"Netlify", "netlify", []string{"--version"}},
+	{"Railway", "railway", []string{"--version"}},
+	{"Render", "render", []string{"--version"}},
+	{"Cloudflare", "wrangler", []string{"--version"}},
+	{"Firebase", "firebase", []string{"--version"}},
 }
+
 
 // ─────────────────────────────────────────────────────────────
 // Version detection
